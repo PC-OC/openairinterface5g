4d6d0869e923ec0daedc98c8e08f597fcec6b050
Laurent THOMAS <laurent.thomas@open-cells.com>
2026-07-17 16:36:23 +0200

    add mutex check option

--- common/utils/utils.h
+++ common/utils/utils.h
@@ -17,10 +17,24 @@ extern "C" {
 #include <sys/types.h>
 #include <common/utils/assertions.h>
 
-#ifdef MALLOC_TRACE
-#define malloc myMalloc
+  #define  CHECK_MUTEX
+#ifdef CHECK_MUTEX
+  #include <pthread.h>
+#define pthread_mutex_init(MuT,mutexattr) ({                            \
+      int macroRet;                                                          \
+      if (mutexattr)                                                    \
+        macroRet= (pthread_mutex_init)((MuT),mutexattr);                     \
+      else {                                                            \
+        pthread_mutexattr_t type;                                       \
+        pthread_mutexattr_init(&type);                                  \
+        pthread_mutexattr_settype(&type,PTHREAD_MUTEX_ERRORCHECK_NP );  \
+        macroRet= (pthread_mutex_init)((MuT),&type);                         \
+      }                                                                 \
+      AssertFatal(!macroRet, "mutex init failed: %s\n",strerror(macroRet));       \
+      macroRet;                                                              \
+    })
 #endif
-
+  
 #define sizeofArray(a) (sizeof(a)/sizeof(*(a)))
 #define CHECK_INDEX(ARRAY, INDEX) assert((INDEX) < sizeofArray(ARRAY))
 
