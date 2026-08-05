1b8547cf3ab909c80665be3fd05bc90c1519841e
Calvin Peyron <calvin.peyron@open-cells.com>
2026-08-05 22:37:20 +0200

    Remove usage of global variable RC and use ITTI instead inside telnet
    Signed-off-by: Calvin Peyron <calvin.peyron@open-cells.com>

---
+++ openair2/RRC/NR/rrc_telnet_tasks.h
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-CSSL-1.0
+ */
+
+#ifndef RRC_TELNET_TASKS_H_
+#define RRC_TELNET_TASKS_H_
+
+#include "nr_rrc_defs.h"
+#include "intertask_interface.h"
+#include "rrc_gNB_UE_context.h"
+#include "rrc_gNB_mobility.h"
+#include "ngap_gNB_ue_context.h"
+#include "rrc_gNB_du.h"
+#include "ran_context.h"
+#include "rrc_gNB_UE_context.h"
+#include "rrc_gNB_NGAP.h"
+
+void rrc_get_single_ue_rnti(MessageDef *msg_p, instance_t instance);
+void rrc_get_ue_context_by_ue_id(MessageDef *msg_p, instance_t instance);
+void rrc_get_du_id_by_ue_id(MessageDef *msg_p, instance_t instance);
+void rrc_get_ue_context_by_rnti_any_du(MessageDef *msg_p, instance_t instance);
+void rrc_trigger_ho_f1(MessageDef *msg_p, instance_t instance);
+void rrc_trigger_ho_n2(MessageDef *msg_p, instance_t instance);
+void rrc_get_ngap_ue_id(MessageDef *msg_p, instance_t instance);
+void rrc_check_ue_context(MessageDef *msg_p, instance_t instance);
+void rrc_gnb_generate_rrcrelease(MessageDef *msg_p, instance_t instance);
+void rrc_gnb_generate_rrcrelease_all(MessageDef *msg_p, instance_t instance);
+void rrc_gnb_trigger_ue_context_release_req(MessageDef *msg_p, instance_t instance);
+
+#endif /* RRC_TELNET_TASKS_H_ */
